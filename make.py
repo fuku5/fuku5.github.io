4830d900e74f61d871bfce149680234ef0038c63
fuku5 <tokyo@keio.jp>
2022-02-24 10:19:22 +0900

    fix visibility

--- make.py
+++ make.py
@@ -6,17 +6,18 @@ import mybib
 publications = mybib.load()
 #print(publications)
 
-paths = Path('templates').glob('*.md')
+paths = Path('_templates').glob('*.md')
 
 for path in paths:
     data = path.open().read()
-    data = '<!- DON\'T EDIT THIS FILE -->\n' \
-            + '<!- EDIT templates/index.md and run make.py -->\n' \
+    data = '<!-- DON\'T EDIT THIS FILE -->\n' \
+            + '<!-- EDIT templates/index.md and run make.py -->\n' \
             + data
     for tag in re.findall(r'\\cite{.*}', data):
         bib_id = re.search(r'cite{(.*)}', data).group(1)
         data = data.replace(tag, publications[bib_id])
 
+    print('load {}'.format(path))
     with open('./{}'.format(path.name), 'w') as f:
         f.write(data)
     print('dump: {}'.format(path.name))
